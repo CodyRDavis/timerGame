5561a463d1b85d4994ddd5dfb6d09eda7a1f9670
Cody Davis <Cody.1337@gmail.com>
2018-10-31 16:48:01 -0500

    updated drawRoom function. it now updates the HTML elements on the page and itterates through the array of choices each room posses and shows it on the screen. this blueprint will allow rooms to quickly be built and that the UI will handle those changes accordingly. drawing the room also attaches event listeners to each of the list items being used for navigation and usng the id assigned toeach li allows the next room to be not only chosen, but pass the information on to the drawfunctions to get the correct room.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,57 @@
-
+//object holding all the rooms and their values
 var rooms = {
     BlankRoom: {
         name: "blank",
         description: "its a blank room with nothing in it",
-        choices
-
+        //choices defines which rooms player can access from this room
+        choices: [
+            {
+                name: "Blank Room",
+                key: "BlankRoom"
+            },
+            {
+                name: 'Blank Hallway',
+                key: "BlankRoom"
+            }
+        ]
     }
 }
 var player = {
     whatRoom: "BlankRoom"
 }
 
+function playerUpdate() {
+    var destination = this.id;
+
+    console.log("destination: "+destination);
+    //player.whatRoom = destination;
+    //drawRoom();
+}
+
 function drawRoom(){
-    console.log(rooms[player.whatRoom]);
+    //variables for ease of reading
+    var roomName = document.getElementById('roomName');
+    var roomDescription = document.getElementById('roomDescription');
+    var navigationOptions = document.getElementById('pathOptions');
+
+    var currentRoom = rooms[player.whatRoom];
+
+    //updating the text on gameboard
+    roomName.textContent= currentRoom.name;
+    roomDescription = currentRoom.description;
+
+    var list = document.createElement("ul");
+    for (var option in currentRoom.choices) {
+        var availableOption = document.createElement("li")
+        availableOption.textContent = currentRoom.choices[option].name;
+        availableOption.id = currentRoom.choices[option].key;
+        //adding in click listener and adding it to the option. the key is the name of the room that can be found in Rooms Object.
+        availableOption.addEventListener("click", playerUpdate);
+
+        list.appendChild(availableOption);
+    }
+    navigationOptions.innerHTML = "";//clears out anything there
+    navigationOptions.append(list);
 };
 
 drawRoom();
