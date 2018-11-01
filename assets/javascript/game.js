6c3a4f987b23f46c80d66fcf4ac2820a2c5a250c
Cody Davis <Cody.1337@gmail.com>
2018-11-01 17:48:26 -0500

    fixed some navigation bugs and tested out special event triggers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,83 @@
 //object holding all the rooms and their values
 var rooms = {
-    blankroom: {
-        name: "blank",
-        description: "its a blank room with nothing in it",
-        image: "blank",
+    bedroom: {
+        name: "Bedroom",
+        description: "It's an old musty bedroom. it looks like the bed has some stains on it...is that blood?",
+        image: "blank", //TODO
         requiredItem: "none",
         //choices defines which rooms player can access from this room
         choices: [
             {
-                name: "Go through the blue door",
-                key: "blueroom"
+                name: "Open the door to the hallway", key: "hallway-bedroom"
             },
             {
-                name: 'Go through the red door',
-                key: "redroom"
+                name: "Open the door to the bathroom", key: "bathroom"
+            },
+            {
+                name:"Look at the the bed.", key: "onBed"
+            },
+            {
+                name: "check under the bed", key: "underbed"
             }
         ]
     },
-    redroom: {
-        name: "Red Room",
-        description: "its a red room...with lots of red things...tacky",
-        image: "redroom.png",
+    bathroom: {
+        name: "Bathroom - connected to the bedroom",
+        description: "It kinda smells in here- I don't think this has been cleaned in a long time.",
+        image: "blank",//TODO
         requiredItem: "none",
-        //choices defines which rooms player can access from this room
         choices: [
             {
-                name: "The white looking room",
-                key: "blankroom"
-            },
-            {
-                name: 'The room with the blue wall',
-                key: "blueroom"
-            },
-            {
-                name: 'pick up the blue key on the floor.',
-                key: 'getBlueKey'
+                name: "Go back to the bedroom",
+                key: "bedroom"
             }
         ]
     },
-    getBlueKey: {
-        name: "Red Room",
-        description: "you pick up the blueKey. it feels cool.",
-        image: "redroom.png",
+    underbed :
+    {
+        name: "looking under the Bed",
+        description: "It's so dusty under here, I'll be sneezing for hours...",
+        image: "blank", //TODO
         requiredItem: "none",
-        //choices defines which rooms player can access from this room
         choices: [
             {
-                name: "go back....",
-                key: "redroom"
+                name: "Get out from under the bed.",
+                key: "bedroom"
             }
         ]
     },
-    blueroom: {
-        name: "Blue Room",
-        description: "its a realy really blue room with nothing in it",
-        image: "blueroom.jpg",
-        requiredItem: "bluekey",
-        //choices defines which rooms player can access from this room
+    onBed: {
+        name: "Looking at the bed",
+        description: "This bed has a big stain on it... gross",
+        image: "NONE", //TODO
+        requiredItem: "none",
         choices: [
             {
-                name: "go back into the white room",
-                key: "blankroom"
-            },
-            {
-                name: 'go through the door into the red room',
-                key: "redroom"
+                name: "go back",
+                key: "bedroom"
             }
-        ]
-    }
-}
+        ]//end of choices
+    }//end of room
+
+}//end of all the rooms
+
 var player = {
-    whatRoom: "blankroom",
+    whatRoom: "bedroom",
+    //flags for special events
+    awake: false,
+    note: false,
+
+    //items to open and get further in the game and 
     none: true,
     bluekey: false,
     "skullkey": false,
     "rustykey": false,
 }
+
+var roomName = document.getElementById('roomName');
+var roomDescription = document.getElementById('roomDescription');
+var navigationOptions = document.getElementById('pathOptions');
+var roomImage = document.getElementById('roomImage');
 
 function playerUpdate() {
 
@@ -88,16 +90,19 @@ function playerUpdate() {
         player.whatRoom = destination;
         drawRoom();
         eventChecker();
+    } else {
+        roomDescription.textContent = "Hm.... I don't seem able to get into this room. Maybe if I had a "+requiredItems+".";   
     }
+    console.log (player);
         
 }
 
 function drawRoom(){
     //variables for ease of reading
-    var roomName = document.getElementById('roomName');
-    var roomDescription = document.getElementById('roomDescription');
-    var navigationOptions = document.getElementById('pathOptions');
-    var roomImage = document.getElementById('roomImage');
+    // var roomName = document.getElementById('roomName');
+    // var roomDescription = document.getElementById('roomDescription');
+    // var navigationOptions = document.getElementById('pathOptions');
+    // var roomImage = document.getElementById('roomImage');
 
     var currentRoom = rooms[player.whatRoom];
 
@@ -106,7 +111,8 @@ function drawRoom(){
     roomDescription.textContent = currentRoom.description;
     roomImage.setAttribute('src', './assets/images/'+currentRoom.image);
 
-    var list = document.createElement("ul");
+    var list = document.createElement("ul")
+    list.id = "nav";
     for (var option in currentRoom.choices) {
         var availableOption = document.createElement("li")
         availableOption.textContent = currentRoom.choices[option].name;
@@ -121,9 +127,18 @@ function drawRoom(){
 };
 
 function eventChecker(){
-   if (player.destination == getBlueKey){
-       player.bluekey = true;
-   }
+    var currentLocation = player.whatRoom;
+    if(currentLocation == "onBed" &&!player.note){//checking to see if player has the note from the bed yet
+        drawSpecialEvent("Take the note off the bed.", "noteOnBed");
+    }
 }
 
+function drawSpecialEvent(text, id) {
+    var availableOption = document.createElement("li")
+    availableOption.textContent = text;
+    availableOption.id = id;
+    //adding in click listener and adding it to the option. the key is the name of the room that can be found in Rooms Object.
+    availableOption.addEventListener("click", playerUpdate);
+    document.getElementById('nav').appendChild(availableOption);
+}
 drawRoom();
